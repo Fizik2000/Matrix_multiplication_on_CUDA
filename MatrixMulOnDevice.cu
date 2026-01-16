734839ea6f2f66e79ef42f3265eda4d30fba5c85
Fizik2000 <sirazhudingasanov3@gmail.com>
2026-01-16 14:54:12 +0300

    finally final

--- MatrixMulOnDevice.cu
+++ MatrixMulOnDevice.cu
@@ -1,20 +1,23 @@
 #include <cuda_runtime.h>
+#include "stdio.h"
 
 __global__ void MatrixMulKernel (double *Ad, double *Bd, double *Cd, int height, int width, int common_number) {
   unsigned i = blockDim.x*blockIdx.x + threadIdx.x,
   j = blockDim.y*blockIdx.y + threadIdx.y;
+  double sum = 0;
 
-  __shared__ double A_shared[484];
-  __shared__ double B_shared[484];
-  A_shared[i*common_number + j] = Ad[i*common_number + j];
-  B_shared[i*width + j] = Bd[i*width + j];
-  __syncthreads();
-  Cd[i * width + j] = 0.0;
   for (int k=0; k < common_number; ++k)
-    Cd[i * width + j] += A_shared[i * common_number + k] * B_shared[k * width + j];
+    sum += Ad[i * common_number + k] * Bd[k * width + j];
+  Cd[i * width + j] = sum;
 }
 
 void MatrixMulOnDevice(double **A, double **B, double **C, int height, int width, int common_number) {
+  cudaEvent_t start, stop;
+  float milliseconds = 0;
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  
   int sizeA = height * common_number * sizeof(double);
   int sizeB = common_number * width * sizeof(double);
   int size = height * width * sizeof(double);
@@ -29,13 +32,25 @@ void MatrixMulOnDevice(double **A, double **B, double **C, int height, int width
 
   dim3 blockSize(22, 22);
   dim3 dimGrid(height/blockSize.x + 1, width/blockSize.y +1);
-  dim3 dimBlock(height, width);
-  MatrixMulKernel<<<dimGrid, dimBlock>>>(Ad, Bd, Cd, height, width, common_number);
+  //dim3 dimBlock(height, width);
+
+  cudaEventRecord(start);
+
+  MatrixMulKernel<<<dimGrid, blockSize>>>(Ad, Bd, Cd, height, width, common_number);
   cudaDeviceSynchronize();
 
+  cudaEventRecord(stop);
+
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&milliseconds, start, stop);
+
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
 
   cudaMemcpy(C[0], Cd, size, cudaMemcpyDeviceToHost);
 
   cudaFree(Ad); cudaFree(Bd); cudaFree(Cd);
+
+  printf("Time of GPU processing: %f\n", milliseconds);
 
 }
